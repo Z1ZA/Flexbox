a81fd8f7f37bb299962e8773c97966f057bfe57f
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-03-29 23:45:24 +0700

    feat : hero section not fix ada bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      padding: "40px",
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#E73330",
+        neutral: {
+          100: "#fafafa",
+          200: "#818181",
+          300: "#212121",
+          400: "#2b2b2b",
+          500: "#000000",
+        },
+        hijau: {
+          500: "#D4F28D",
+          300: "#3DCE30",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito sans", "sans-serif"],
+        heading: ["Bebas Neue", "sans-serif"],
+        koulen: ["Koulen", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
