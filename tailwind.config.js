b2fa03503d6ea4e3e51ce2938e075841173c70fc
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-04-01 05:25:22 +0700

    feat: services & pricing done

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,8 @@ module.exports = {
           600: "#202020",
         },
         hijau: {
-          500: "#D4F28D",
-          300: "#3DCE30",
+          400: "#3DCE30",
+          300: "#B6F329",
         },
       },
       fontFamily: {
@@ -27,6 +27,7 @@ module.exports = {
         heading: ["Bebas Neue", "sans-serif"],
         koulen: ["Koulen", "sans-serif"],
         jakarta: ["Plus Jakarta Sans", "sans-serif"],
+        jam: ["Inter", "sans-serif"],
       },
     },
   },
