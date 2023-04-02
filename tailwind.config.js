64dcebfa5f0ef0599add8a5efe7b9d8217ffc284
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-04-03 01:21:15 +0700

    feat: inquiry not fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "inquiry.html"],
   theme: {
     container: {
       padding: "40px",
